df3683a52dd08dbfd47f2733105ffd15adb22e48
Debora <deboraro.61@gmail.com>
2020-05-22 10:03:34 -0300

    Inserindo o object destructuring na criação do componente card

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -2,12 +2,15 @@
 //qdo usado a arrow function e só possui uma linha não é necessário o uso da palavra chave return e nem chaves
 //a função omite eles, porem o resultado é como se usassemos;
 /*card recebe o argumento em JSON e acessa suas propriedades com o card.*/
-const createMemoryCard = (card) => `
-        <article class="memory-card ${card.nameClass}">
+const createMemoryCard = card => {
+        const {src, alt, nameClass} = card;
+        return`
+        <article class="memory-card ${nameClass}">
                 <img 
-                        src="${card.src}"
-                        alt="${card.alt}" 
+                        src="${src}"
+                        alt="${alt}" 
                         class='icon' onclick="handleClick()">
                 </article>`;
+        };
 
 
