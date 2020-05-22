8f08b3b2c296992b2e5f57d0d2c1f32b3ededbd1
Debora <deboraro.61@gmail.com>
2020-05-22 10:46:35 -0300

    Inserindo o css diretamente no js

--- src/objects/cards-wrapper/index.js
+++ src/objects/cards-wrapper/index.js
@@ -1,8 +1,22 @@
 //gera a section com o DOM para tornar-se um element
 function createCardsWrapper(){
     const $cardsWrapper = document.createElement("section")
-
     $cardsWrapper.classList.add("cards-wrapper");
+
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");//cria a tag style
+    $style.textContent = `.cards-wrapper{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        padding-top: 10px;
+    }
+    
+    .cards-wrapper > .memory-card{
+        margin-bottom: 10px;
+    }`;/*cria o conteudo para a tag style*/
+    
+    $head.insertBefore($style, null);
 
     return $cardsWrapper;
 }
