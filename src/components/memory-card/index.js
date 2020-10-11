bf7e8b9de870d75ecc59202526b35da80d782dc6
Debora <deboraro.61@gmail.com>
2020-10-10 21:00:02 -0300

    JavaScript para fazer a coisa acontecer

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -5,56 +5,78 @@
 
 
 const createMemory = () => {
-    console.log("a função foi executada")
     const $head = document.querySelector("head");
     const $style = document.createElement("style");/*cria o elemento*/
     $style.textContent = `
         .memory-card{
-        width: 155px;
-        height: 155px;
+            position relative;
+            width: 155px;
+            height: 155px;
+        }
+        .memory-card .card{
+        width:100%;
+        height:100%;
         background-color: #f25a70;
         border-radius: 30px;
         display: flex;
         justify-content: center;
         align-items: center;
         box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
-        position: relative;
         cursor: pointer;
-    }
-    .memory-card.-front{
-        background-color: #2a2a2a;
-    }
-    .memory-card.-front::before{
-        content: "";
-        width: 95px;
-        height: 95px;
-        background-color: #d4d4d4;
-        border-radius: 50%;
-        position: absolute;   
-    }
-    .memory-card > .icon{
-        width: 100px;
-        height: 100px;
-    }
-    
-    .memory-card.-front > .icon{
-        position: absolute;
-        transform: translateY(-10px);
-    }
-`;
+        position absolute;
+        margin-bottom:10px;
+        }
+        .memory-card .card.-front{
+            background-color: #fff000;
+            display:none;
+        }
+        .memory-card.-active .card{
+            display:none;
+        }
+        .memory-card.-active .-front{
+            display:flex;
+        }
+        .memory-card .card.-front::before{
+            content: "";
+            width: 95px;
+            height: 95px;
+            background-color: #d4d4d4;
+            border-radius: 50%;
+            position: absolute;   
+        }
+        .memory-card .card > .icon{
+            width: 100px;
+            height: 100px;
+        }
+        .memory-card .card.-front > .icon{
+            position: absolute;
+            transform: translateY(-10px);
+        }
+    `;
 $head.insertBefore($style, null);/*insere o style dentro dele*/ 
 
 
-return ({src, alt, nameClass})=>`
-    <article class="memory-card ${nameClass}">
-        <img 
-            src="${src}"
-            alt="${alt}" 
-            class='icon' onclick="handleClick()">
-    </article>`;
-};
+return ({src, alt})=>`
+    <div class="memory-card" onclick="handleClick(this)">
+            <article class="card -front">
+                <img 
+                    src="${src}"
+                    alt="${alt}" 
+                    class='icon'>
+            </article>
+            <article class="card">
+                <img 
+                    src="../img/icon-collabcode.png"
+                    alt="icone do gueio" 
+                    class='icon'>
+            </article>
+    </div>
+    `;
+}
 
-
+const handleClick = function($this){//não podemos usar a palavra this (keyword)
+    $this.classList.toggle("-active");
+}
 
 
 
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -6,24 +6,24 @@ const $cardsWrapper = createCardsWrapper();//section c/ class
 const createMemoryCard = createMemory();/*aqui voce executa a função e atribui seu retorno a variavel*/ 
 
 const $memoryCardJS = createMemoryCard( {/*passando um objeto como argumento*/ 
-    nameClass: "-front",
     src: "img/icon-js.png",
-    alt: "Icone da linguagem Javascript"
+    alt: "Icone da linguagem Javascript",
 });
 
 const $memoryCardC = createMemoryCard( {
-    nameClass: "-front",
     src: "img/icon-c.png",
     alt: "Icone da linguagem C"
 });
 
 const $memoryCardWoman = createMemoryCard( {
-    nameClass: "-front",
     src: "img/icon-woman.png",
-    alt: "Icone da linguagem C"
+    alt: "Icone da garota"
 });
 
-
+const $memoryCardPhone = createMemoryCard( {
+    src: "img/icon-celular.png",
+    alt: "Icone do celular"
+});
 
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
@@ -31,6 +31,8 @@ $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardWoman);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardWoman);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardPhone);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardPhone);
 
 $root.insertAdjacentElement("beforeend", $cardsWrapper); //add section ao root
 
